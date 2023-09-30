4d498d092a7be6ea967694a496caf5cf7174c4ea
coolguy284 <coolguy284v64@gmail.com>
2023-09-30 01:19:38 -0400

    add load rawdata from file button
    clean up download to file code
    
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- func_raw_data.js
+++ func_raw_data.js
@@ -44,14 +44,36 @@ function rawDataLoadInMemoryData() {
 }
 
 function rawDataDownloadToFile() {
-  let element = document.createElement('a');
+  let anchorTag = document.createElement('a');
   
-  element.setAttribute('href', 'data:application/octet-stream;base64,' + btoa(raw_data_text.value));
+  anchorTag.setAttribute('href', 'data:application/octet-stream;base64,' + btoa(raw_data_text.value));
   
-  element.setAttribute('download', 'archived.txt');
+  anchorTag.setAttribute('download', 'archived.txt');
   
-  element.style.display = 'none';
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
+  anchorTag.click();
+}
+
+function rawDataLoadFromFile() {
+  // https://stackoverflow.com/questions/16215771/how-to-open-select-file-dialog-via-js/40971885#40971885
+  
+  let inputTag = document.createElement('input');
+  inputTag.type = 'file';
+  
+  inputTag.onchange = evt => {
+    if (evt.target.files.length == 0) return;
+    
+    let file = evt.target.files[0];
+    
+    let fileReader = new FileReader();
+    
+    fileReader.readAsText(file, 'utf-8');
+    
+    fileReader.onload = readerEvt => {
+      let content = readerEvt.target.result;
+      
+      raw_data_text.value = content;
+    };
+  };
+  
+  inputTag.click();
 }
