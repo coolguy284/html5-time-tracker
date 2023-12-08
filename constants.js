c840f083fed70ff4ae3ec6d1a726d3f89f421b4f
coolguy284 <coolguy284v64@gmail.com>
2023-12-07 20:41:31 -0500

    reorganize constants
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- constants.js
+++ constants.js
@@ -1,15 +1,28 @@
+// event constants
 let EVENT_NOTHING = 'Nothing';
 let EVENT_UNLOGGED = 'Unlogged';
-let DATA_VIEW_ADDL_INFO_BIG_INDENT = false;
 let EVENT_MAPPINGS_DEFAULT_EVENT_GROUP = 'Default';
 let EVENT_MAPPINGS_DEFAULT_EVENT_GROUP_COLOR = 'black';
 let DEFAULT_EVENT_MAPPING = 'Main';
 let MULTI_EVENT_SPLIT = ' | ';
+
+// storage constants
+let LOCALSTORAGE_MAIN_STORAGE_KEY = 'html5_time_planner_events_arr';
+
+// render constants
+let PAGE_MANAGER_HIDE_PAGE_ON_NULL = false;
+
+// page constants > scroll buttons
+let SCROLL_ASSIST_BUTTONS_MIN_SCROLL_HEIGHT = 2000;
+
+// page constants > data page
+let DATA_VIEW_ADDL_INFO_BIG_INDENT = false;
+
+// page constants > charts page
 let TABLE_DATA_FULL_HEIGHT = 30; // in units of rem
 let TABLE_STRIPE_WIDTH = 0.3; // in units of rem
 let STATS_ENTRY_COLOR_BLOCK_WIDTH = 1.5; // in units of rem
 let STATS_ENTRY_GAP_WIDTH = 0.3; // in units of rem
-let LOCALSTORAGE_MAIN_STORAGE_KEY = 'html5_time_planner_events_arr';
-let SCROLL_ASSIST_BUTTONS_MIN_SCROLL_HEIGHT = 2000;
+
+// page constants > extras > raw data page
 let RAW_DATA_PRETTIFY_LINE_LIMIT = 140;
-let PAGE_MANAGER_HIDE_PAGE_ON_NULL = false;
