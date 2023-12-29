dc073bdf05b963fa2def1c528d1180f191886be5
coolguy284 <coolguy284v64@gmail.com>
2023-12-28 20:54:39 -0500

    add text ish mode printing to edit page
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- scripts/func_edit.js
+++ scripts/func_edit.js
@@ -74,10 +74,18 @@ let setPseudoRawData = asyncManager.wrapAsyncFunctionWithButton(
   async () => {
     let parsedJson;
     
-    try {
-      parsedJson = JSON.parse(pseudo_raw_data_text.value);
-    } catch {
-      alert('JSON invalid');
+    switch (render_mode.value) {
+      case 'JSON':
+        try {
+          parsedJson = JSON.parse(pseudo_raw_data_text.value);
+        } catch {
+          alert('JSON invalid');
+        }
+        break;
+      
+      case 'Text-ish':
+        parsedJson = {};
+        break;
     }
     
     await setPseudoRawDataInStorage(parsedJson);
@@ -88,7 +96,87 @@ let reloadPseudoRawData = asyncManager.wrapAsyncFunctionWithButton(
   'reloadPseudoRawData',
   reload_pseudo_raw_data_btn,
   async () => {
-    pseudo_raw_data_text.value = prettifyJson(await getPseudoRawDataFromStorage());
+    let storageData = await getPseudoRawDataFromStorage();
+    
+    let textValue;
+    
+    switch (render_mode.value) {
+      case 'JSON':
+        textValue = prettifyJson(storageData);
+        break;
+      
+      case 'Text-ish':
+        textValue = '';
+        
+        if ('eventButtons' in storageData) {
+          textValue += `eventButtons:\n${prettifyJson(storageData.eventButtons)}\n\n`;
+        }
+        
+        if ('eventPriorities' in storageData) {
+          textValue += `eventPriorities:\n${prettifyJson(storageData.eventPriorities)}\n\n`;
+        }
+        
+        if ('eventMappings' in storageData) {
+          textValue += `eventMappings:\n${prettifyJson(storageData.eventMappings)}\n\n`;
+        }
+        
+        if ('events' in storageData) {
+          textValue += 'events:\n';
+          let trueEvents;
+          
+          if (typeof storageData.events[0] == 'string') {
+            trueEvents = storageData.events.slice(1);
+            textValue += `${storageData.events[0]}\n`;
+          } else {
+            trueEvents = storageData.events;
+          }
+          
+          let pastDay = null;
+          let pastTZ = null;
+          
+          for (let event of trueEvents) {
+            let tsSplit = event[0].split(' ');
+            let day = tsSplit[0];
+            let tz = tsSplit[3];
+            
+            if (day != pastDay) {
+              if (tz != pastTZ) {
+                textValue += `${day} ${tz}:\n`;
+              } else {
+                textValue += `${day}:\n`;
+              }
+            } else {
+              if (tz != pastTZ) {
+                textValue += `${tz}:\n`;
+              } else {
+                // nothing
+              }
+            }
+            
+            pastDay = day;
+            pastTZ = tz;
+            
+            textValue += `${tsSplit[1]} ${tsSplit[2]} ${event[2] ? '1' : '0'}${event[3] ? '1' : '0'}`;
+            
+            if (event[1].includes('\n')) {
+              textValue += `${JSON.stringify(event[1])}\n`;
+            } else {
+              textValue += ` ${event[1]}\n`;
+            }
+            
+            if (event.length > 4) {
+              if (event[4].includes('\n')) {
+                textValue += ` ${JSON.stringify(event[4])}\n`;
+              } else {
+                textValue += `  ${event[4]}\n`;
+              }
+            }
+          }
+        }
+        break;
+    }
+    
+    pseudo_raw_data_text.value = textValue.trim() + '\n';
   }
 );
 
