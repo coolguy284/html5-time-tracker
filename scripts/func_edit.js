ba24e9042ee94364417f1ee1da92b6c68406951a
coolguy284 <coolguy284v64@gmail.com>
2023-12-28 21:14:41 -0500

    work on text-ish mode printing
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- scripts/func_edit.js
+++ scripts/func_edit.js
@@ -68,23 +68,124 @@ async function setPseudoRawDataInStorage(pseudoRawData) {
   }
 }
 
+function editViewTextToObject(text) {
+  let match = /^(?:eventButtons:\n(.*)\n\n)?(?:eventPriorities:\n(.*)\n\n)?(?:eventMappings:\n(.*)\n\n)?(?:events:\n(.*))?$/.exec(text.trim());
+  
+  if (match) {
+    let obj = {};
+    
+    return obj;
+  } else {
+    return null;
+  }
+}
+
+function editViewObjectToText(obj) {
+  let textValue = '';
+  
+  if ('eventButtons' in obj) {
+    textValue += `eventButtons:\n${prettifyJson(obj.eventButtons)}\n\n`;
+  }
+  
+  if ('eventPriorities' in obj) {
+    textValue += `eventPriorities:\n${prettifyJson(obj.eventPriorities)}\n\n`;
+  }
+  
+  if ('eventMappings' in obj) {
+    textValue += `eventMappings:\n${prettifyJson(obj.eventMappings)}\n\n`;
+  }
+  
+  if ('events' in obj) {
+    textValue += 'events:\n';
+    let trueEvents;
+    
+    if (typeof obj.events[0] == 'string') {
+      trueEvents = obj.events.slice(1);
+      textValue += `${obj.events[0]}\n`;
+    } else {
+      trueEvents = obj.events;
+    }
+    
+    let pastDay = null;
+    let pastTZ = null;
+    
+    for (let event of trueEvents) {
+      let tsSplit = event[0].split(' ');
+      let day = tsSplit[0];
+      let tz = tsSplit[3];
+      
+      if (day != pastDay) {
+        if (tz != pastTZ) {
+          textValue += `${day} ${tz}:\n`;
+        } else {
+          textValue += `${day}:\n`;
+        }
+      } else {
+        if (tz != pastTZ) {
+          textValue += `${tz}:\n`;
+        } else {
+          // nothing
+        }
+      }
+      
+      pastDay = day;
+      pastTZ = tz;
+      
+      textValue += `${tsSplit[1]} ${tsSplit[2]} ${event[2] == null ? '-' : event[2] ? '1' : '0'}${event[3] == null ? '-' : event[3] ? '1' : '0'}`;
+      
+      if (event[1].includes('\n')) {
+        textValue += `${JSON.stringify(event[1])}\n`;
+      } else {
+        textValue += ` ${event[1]}\n`;
+      }
+      
+      if (event.length > 4) {
+        if (event[4].includes('\n')) {
+          textValue += ` ${JSON.stringify(event[4])}\n`;
+        } else {
+          textValue += `  ${event[4]}\n`;
+        }
+      }
+    }
+  }
+  
+  return textValue.trim() + '\n';
+}
+
 let setPseudoRawData = asyncManager.wrapAsyncFunctionWithButton(
   'setPseudoRawData',
   set_pseudo_raw_data_btn,
   async () => {
+    let inputText = pseudo_raw_data_text.value;
+    
     let parsedJson;
     
     switch (render_mode.value) {
       case 'JSON':
         try {
-          parsedJson = JSON.parse(pseudo_raw_data_text.value);
+          parsedJson = JSON.parse(inputText);
         } catch {
           alert('JSON invalid');
+          return;
         }
         break;
       
       case 'Text-ish':
-        parsedJson = {};
+        if (inputText[0] == '{') {
+          try {
+            parsedJson = JSON.parse(inputText);
+          } catch {
+            alert('JSON invalid');
+            return;
+          }
+        } else {
+          parsedJson = editViewTextToObject(inputText);
+          
+          if (parsedJson == null) {
+            alert('Text form invalid');
+            return;
+          }
+        }
         break;
     }
     
@@ -106,77 +207,14 @@ let reloadPseudoRawData = asyncManager.wrapAsyncFunctionWithButton(
         break;
       
       case 'Text-ish':
-        textValue = '';
-        
-        if ('eventButtons' in storageData) {
-          textValue += `eventButtons:\n${prettifyJson(storageData.eventButtons)}\n\n`;
-        }
-        
-        if ('eventPriorities' in storageData) {
-          textValue += `eventPriorities:\n${prettifyJson(storageData.eventPriorities)}\n\n`;
-        }
-        
-        if ('eventMappings' in storageData) {
-          textValue += `eventMappings:\n${prettifyJson(storageData.eventMappings)}\n\n`;
-        }
-        
-        if ('events' in storageData) {
-          textValue += 'events:\n';
-          let trueEvents;
-          
-          if (typeof storageData.events[0] == 'string') {
-            trueEvents = storageData.events.slice(1);
-            textValue += `${storageData.events[0]}\n`;
-          } else {
-            trueEvents = storageData.events;
-          }
-          
-          let pastDay = null;
-          let pastTZ = null;
-          
-          for (let event of trueEvents) {
-            let tsSplit = event[0].split(' ');
-            let day = tsSplit[0];
-            let tz = tsSplit[3];
-            
-            if (day != pastDay) {
-              if (tz != pastTZ) {
-                textValue += `${day} ${tz}:\n`;
-              } else {
-                textValue += `${day}:\n`;
-              }
-            } else {
-              if (tz != pastTZ) {
-                textValue += `${tz}:\n`;
-              } else {
-                // nothing
-              }
-            }
-            
-            pastDay = day;
-            pastTZ = tz;
-            
-            textValue += `${tsSplit[1]} ${tsSplit[2]} ${event[2] ? '1' : '0'}${event[3] ? '1' : '0'}`;
-            
-            if (event[1].includes('\n')) {
-              textValue += `${JSON.stringify(event[1])}\n`;
-            } else {
-              textValue += ` ${event[1]}\n`;
-            }
-            
-            if (event.length > 4) {
-              if (event[4].includes('\n')) {
-                textValue += ` ${JSON.stringify(event[4])}\n`;
-              } else {
-                textValue += `  ${event[4]}\n`;
-              }
-            }
-          }
+        textValue = editViewObjectToText(storageData);
+        if (!deepEqual(editViewTextToObject(textValue), storageData)) {
+          //textValue = prettifyJson(storageData);
         }
         break;
     }
     
-    pseudo_raw_data_text.value = textValue.trim() + '\n';
+    pseudo_raw_data_text.value = textValue;
   }
 );
 
--- scripts/lib/deep_obj_funcs.js
+++ scripts/lib/deep_obj_funcs.js
@@ -24,3 +24,41 @@ function deepClone(obj) {
       throw new Error('cannot deep clone function');
   }
 }
+
+function deepEqual(obj1, obj2) {
+  if (typeof obj1 != typeof obj2) {
+    return false;
+  }
+  
+  if (typeof obj1 != 'object' && typeof obj1 != 'function' || obj1 === null) {
+    return Object.is(obj1, obj2);
+  }
+  
+  if (typeof obj1 == 'function' || typeof obj2 == 'function') {
+    throw new Error('cannot deep equals a function');
+  }
+  
+  if (Array.isArray(obj1)) {
+    if (!Array.isArray(obj2)) {
+      return false;
+    }
+    
+    if (obj1.length != obj2.length) {
+      return false;
+    }
+    
+    for (let i = 0; i < obj1.length; i++) {
+      if (!deepEqual(obj1[i], obj2[i])) {
+        return false;
+      }
+    }
+    
+    return true;
+  } else {
+    if (Array.isArray(obj2)) {
+      return false;
+    }
+    
+    return deepEqual(Object.entries(obj1), Object.entries(obj2));
+  }
+}
