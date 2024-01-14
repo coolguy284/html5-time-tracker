db89f480e137f906dff3fefca5d169e262c602ce
coolguy284 <coolguy284v64@gmail.com>
2024-01-14 00:17:34 -0500

    move some ui funcs into common ui
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- scripts/common.js
+++ scripts/common.js
@@ -162,16 +162,3 @@ function normalizeSlice(start, end, length) {
   
   return [start, end];
 }
-
-function removeAllChildren(elem) {
-  // https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript/3955238#3955238
-  while (elem.firstChild) {
-    elem.removeChild(elem.lastChild);
-  }
-}
-
-function removeAllChildrenButOne(elem) {
-  while (elem.children.length > 1) {
-    elem.removeChild(elem.lastChild);
-  }
-}
